8b8f84679d7e8a8fde263b5a05be7d040ae6264a
Aaryan Kamalwanshi <162993299+Aaryank-47@users.noreply.github.com>
2025-05-02 18:44:26 +0530

    Update urls.py

--- social-media/socialmedia/userauth/urls.py
+++ social-media/socialmedia/userauth/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from socialmedia import settings
+from django.conf import settings
 from userauth import views
 from django.conf.urls.static import static
 from django.db.models import Q
