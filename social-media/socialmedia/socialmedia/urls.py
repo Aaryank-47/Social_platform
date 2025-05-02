5d025194f80bcc8dbf6a12ccb73b8eb0cd565cff
Aaryan Kamalwanshi <162993299+Aaryank-47@users.noreply.github.com>
2025-05-02 18:32:00 +0530

    Update urls.py

--- social-media/socialmedia/socialmedia/urls.py
+++ social-media/socialmedia/socialmedia/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('userauth.urls')),
+    path('',include('socialmedia.userauth.urls')),
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,
