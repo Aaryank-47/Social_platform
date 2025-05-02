cdaac852a8bafd6c715cf808d8ec445f11b7c2a7
Aaryan Kamalwanshi <162993299+Aaryank-47@users.noreply.github.com>
2025-05-02 18:16:28 +0530

    Update settings.py

--- social-media/socialmedia/socialmedia/settings.py
+++ social-media/socialmedia/socialmedia/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'userauth'
+    'socialmedia.userauth'
 ]
 
 MIDDLEWARE = [
